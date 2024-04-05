206413d49766bf614f78e0fbd7f3eb0d24427e39
loganrains <logankrains@gmail.com>
2024-04-04 20:21:01 -0500

    Completed Exercises: Arrays

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,16 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split());
+console.log(str.split('e'));
+// it is split by the parameter
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+console.log(arr.join());
+console.log(arr.join(', '));
+// it is joined with the parameter in betweeen
 //3) Do split or join change the original string/array?
-
+console.log(arr); // No
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+cargohold = cargoHold.split(",").sort().join(", ");
+console.log(cargohold);
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,15 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-
+console.log(holdCabinet1 + holdCabinet2);
+console.log(holdCabinet1); // it does not alter the original arrays
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
-
+console.log(holdCabinet1.slice(0, 2));
+console.log(holdCabinet1); // It doesn't alter the original array
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+console.log(holdCabinet1.reverse());
+console.log(holdCabinet2.sort());
+// both rearange the order of the arrays but in different ways
+console.log(holdCabinet1.reverse());
+console.log(holdCabinet2.sort());
+// sort does change the original
