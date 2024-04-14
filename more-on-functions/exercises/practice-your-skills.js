74112e7a8b4c625f6f7d02a2d3be1159ce18afc6
loganrains <logankrains@gmail.com>
2024-04-13 19:50:42 -0500

    Completed CH 11 Exercises

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,5 +1,12 @@
 //Create an anonymous function and set it equal to a variable.
-
+let practice = function (Arg) {
+    if(typeof(Arg) === "number"){
+        return Arg * 3;
+    } else if (typeof(Arg) === "string"){
+        return "ARRR";
+    }
+    return Arg;
+};
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
@@ -13,3 +20,11 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(practice(arr[i]));
+// }
+
+let mappedArr = arr.map(practice);
+
+console.log(mappedArr);
