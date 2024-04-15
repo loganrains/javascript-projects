fa8e7b8cbb89dede50d9cf2b1af1f2f57e8aa6a4
loganrains <logankrains@gmail.com>
2024-04-15 10:29:15 -0500

    Chapter 17 Exercises complete

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,9 +1,13 @@
 function gradeLabs(labs) {
+  try {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+} catch(err) {
+  console.log("Error thrown");
+}
 }
 
 let studentLabs = [
@@ -18,6 +22,27 @@ let studentLabs = [
     runLab: function (num) {
         return num * num;
     }
+  }
+];
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
   }
 ];
 
